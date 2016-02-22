a3f14cd740b1600d90cb08594e9d4e3e5c15951f
miraks <a@vldkn.net>
2016-02-22 15:36:31 +0300

    object properties test

---
+++ test/fixtures/object-properties/expected.js
@@ -0,0 +1,15 @@
+const obj = {
+  a: function () {
+    return 1;
+  },
+  b: () => {
+    return 2;
+  },
+  c: () => 3,
+  d() {
+    return 4;
+  },
+  ["a" + "b"]: () => {
+    return 5;
+  }
+};
